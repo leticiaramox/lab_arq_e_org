308b5507545e9df7cc52f98c014b82beea8935f9
Letícia Ramos <leticiaramosoliveira0205@gmail.com>
2026-08-25 10:56:52 -0300

    Update atv5.asm

--- Lab_2/atv5.asm
+++ Lab_2/atv5.asm
@@ -1,16 +1,9 @@
-lw x10, v1        # x10 = a 
-lw x11, v2        # x11 = b
+loop:
+lb x10, 1025(x0)
+addi x11, x0, 42
+beq x10, x11, fim
+sb x10, 1024(x0)
+jal x0, loop
 
-sw x10, v3        
-
-bge x11, x10, fim_if
-
-add x12, x10, x11 # x12 = a + b
-sw x12, v3     
-
-fim_if:
+fim:
 halt
-
-v1: .word 14      # int a = 6
-v2: .word 7       # int b
-v3: .word 0       # int m = 0
