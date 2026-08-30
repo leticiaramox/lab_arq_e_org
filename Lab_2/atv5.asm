5fbcc6525667b7124b87c9003bb9a36a037d66d1
Thiago Ewerton <tebp@cin.ufpe.br>
2026-08-29 22:43:42 -0300

    Refactor main loop and add halt instruction
    Removed character printing loop and added a halt instruction.

--- Lab_2/atv5.asm
+++ Lab_2/atv5.asm
@@ -1,13 +1,3 @@
-main:
-    addi x11, x0, 28
-
-loop_print:
-    lb x10, 0(x11)          # le o caractere
-    beq x10, x0, inicio_echo # se for 0, encerra o print do texto
-    sb x10, 1024(x0)        # escreve na tela
-    addi x11, x11, 1        # avanca proximo caractere
-    beq x0, x0, loop_print
-
 inicio_echo:
     addi x12, x0, 42        # ASCII do '*'
 
@@ -21,5 +11,3 @@ loop_echo:
 
 fim:
     halt
-
-str1: .string "Hello World"
